5507726f505bb78c55c5a3d7ee5ecd878de1960a
Miha Zelnik <miha.zelnik@gmail.com>
2022-01-14 10:43:31 +0100

    Flask app

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # sentry-to-discord
+
+Service for sending Sentry errors to your Discord channel
+
+## Heroku
+
+Before deploying to Heroku you should be familiar with the basic concepts of [Git](https://git-scm.com/) and [Heroku](https://heroku.com/).
+
+If you keep your project on GitHub you can use 'Deploy to Heroku' button thanks to which the deployment can be done in web browser with minimal configuration required.
+The configuration used by the button is stored in `app.json` file.
+
+<a href="https://heroku.com/deploy" style="display: block"><img src="https://www.herokucdn.com/deploy/button.svg" title="Deploy" alt="Deploy"></a>
+    <br>
+
+Deployment by using [Heroku CLI](https://devcenter.heroku.com/articles/heroku-cli):
+
+* Create Heroku App. You can leave your app name, change it, or leave it blank (random name will be generated)
+
+    ```bash
+    heroku create sentry-to-discord
+    ```
+
+* Add buildpacks
+
+    ```bash
+    heroku buildpacks:add --index=1 heroku/python
+    ```
+* Set environmental variables (change `SENTRY_SECRET` and `DISCORD_SENTRY_WEBHOOK` value)
+
+    ```bash
+    heroku config:set SENTRY_SECRET=not-so-secret
+    heroku config:set FLASK_APP=app.py
+    heroku config:set DISCORD_SENTRY_WEBHOOK=discord-webhook-url
+    ```
+
+* Deploy on Heroku by pushing to the `heroku` branch
+
+    ```bash
+    git push heroku master
+    ```
---
+++ app.py
@@ -0,0 +1,90 @@
+import os
+
+from discord_webhook import DiscordEmbed, DiscordWebhook
+from flask import Flask, Response
+from flask_bootstrap import Bootstrap
+
+DISCORD_SENTRY_WEBHOOK = os.getenv("DISCORD_SENTRY_WEBHOOK")
+SENTRY_SECRET = os.getenv("SENTRY_SECRET")
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+def get_discord_color(level):
+    if level == "debug":
+        return "fbe14f"
+    elif level == "info":
+        return "2788ce"
+    elif level == "warning":
+        return "f18500"
+    elif level == "fatal":
+        return "d20f2a"
+    else:
+        return "e03e2f"
+
+
+def get_stacktrace(event):
+    if "stacktrace" in event:
+        return event["stacktrace"]
+
+    elif "exception" in event:
+        exception = event["exception"]
+        if exception.get("values"):
+            return exception["values"][0].get("stacktrace")
+    return None
+
+
+def get_error_code_snippet(event):
+    stacktrace = get_stacktrace(event)
+    if not stacktrace:
+        return None
+
+    if not stacktrace.get("frames"):
+        return None
+
+    location = stacktrace["frames"][-1]
+    if not location:
+        return None
+
+    return " {}\n>{}\n {}".format(
+        "\n ".join(location.get("pre_context") or []),
+        location.get("context_line"),
+        "\n ".join(location.get("post_context") or []),
+    )
+
+
+def handle_event(data):
+    webhook = DiscordWebhook(url=DISCORD_SENTRY_WEBHOOK)
+
+    event = data["event"]
+
+    snippet = get_error_code_snippet(event)
+    location_text = ""
+    language = ""
+
+    if location := event.get("location"):
+        location_text = "`{}`\n".format(location)
+        language = location.split(".")[-1]
+
+    snippet_text = "```{}\n{}\n```".format(language, snippet)
+
+    embed = DiscordEmbed(
+        title=event.get("title", "Sentry Event"),
+        url=data["url"],
+        description="{}{}".format(location_text, snippet_text),
+        color=get_discord_color(data["level"]),
+        footer={"text": event.get("environment")},
+    )
+    embed.set_timestamp(event["timestamp"])
+
+    webhook.add_embed(embed)
+    webhook.execute()
+
+
+@app.route("/", methods=["POST"])
+def webhook():
+    if request.GET.get("secret") != SENTRY_SECRET:
+        return Response(None, status=401)
+    handle_event(request.data)
+    return Response(None, status=200)
